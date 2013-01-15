591f74480bad2fd0aafcf855858a0a0018a25c7a
Tulio <tulio.bits@prix.com.br>
2013-01-15 11:10:34 -0200

    Master

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+    <!-- CSS Media Queries -->
+    <script src="js/vendor/css3-mediaqueries.js"></script>
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+    <!-- Plugins and Scripts -->
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+    <!-- Google Analytics -->
+    <script>
+      var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+      (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+      g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+      s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<?php
+  header('X-UA-Compatible: IE=edge,chrome=1');
+?>
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js ie8" lang="pt-br"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="pt-br"> <!--<![endif]-->
+  <head>
+    <!-- Meta and Title -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width">
+    <!-- CSS and Favicon  -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- Modernizr -->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+  </head>
+  <body>
+  <!-- Chromeframe -->
+  <!--[if IE 7]>
+    <div class="row chromeframe"><div class="row-center text-center">O seu navegador está <strong>desatualizado</strong>. Por favor, <a href="http://browsehappy.com/" title="Browse Happy">autalize-o</a> ou <a href="http://www.google.com/chromeframe/?redirect=true" title="Google Chrome Frame">ative o Google Chrome Frame</a> para melhorar sua experiência de navegação.</div></div>
+  <![endif]-->
